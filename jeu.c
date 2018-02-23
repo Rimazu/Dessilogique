7df6e59a93f495dc83b3aac0aaa457f04fb22c39
Méridium <thomas.caillou@laposte.net>
2018-02-23 19:12:29 +0100

    Changements mineurs

--- jeu.c
+++ jeu.c
@@ -4,14 +4,14 @@
 
 
 int** stockage(int** grille, char * nom,int* m,int* n) {
-	
+
 	FILE * fichier;
-	
+
 	fichier = fopen(nom,"r");
 	if (fichier) {
-		&m = fgets(fichier);
-		&n = fgets(fichier);
-		
+		fscanf(fichier,&n);
+		fscanf(fichier,&m);
+
 		for (i=0;i<*m;i++) {
 			for (j=0;j<*n;j++) {
 				fscanf(fichier, "%s", grille[i][j]);
@@ -19,59 +19,8 @@ int** stockage(int** grille, char * nom,int* m,int* n) {
 		}
 		fclose(fichier);
 	}
-	
+
 	return(grille);
-	
+
 }
 
-
-int * IndiceLignes(int n,int m,int ** grille) {
-    int i=0,j=0,indice=0,compteur=0;
-    int indicelign[n];
-
-    for (i=0;i<n;i++) {
-        j=0;
-        while (j<m) {
-            indice=0;
-
-            while (j<m)&&(0==grille[i][j]) {
-                j++;
-            }
-
-            while (j<m)&&(1==grille[i][j]) {
-                j++;
-                indice++;
-            }
-            if (0!=indice) {
-                indicelign[compteur]=indice;
-            }
-        }
-    }
-    return indicelign;
-}
-
-int * IndiceColonnes(int n,int m,int ** grille) {
-    int i=0;,j=0,indice=0,compteur=0;
-    int indicecol[m];
-
-    for (j=0;j<m;j++) {
-        i=0;
-        while (i<n) {
-            indice=0;
-
-            while (i<n)&&(0==grille[i][j]) {
-                i++;
-            }
-
-            while (i<n)&&(1==grille[i][j]) {
-                i++;
-                indice++;
-            }
-            if (0!=indice) {
-                indicecol[compteur]=indice;
-                compteur++;
-            }
-        }
-    }
-    return indicecol;
-}
--- jeu.h
+++ jeu.h
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 //renvoie le tableau qui contient les indices des lignes de la grille
-int * IndiceLignes(int n,int m,int ** grille);
+int ** IndiceLignes(int n,int m,int ** grille);
 
 //renvoie le tableau qui contient les indices des colonnes de la grille
-int * IndiceColonnes(int n,int m,int ** grille);
+int ** IndiceColonnes(int n,int m,int ** grille);
