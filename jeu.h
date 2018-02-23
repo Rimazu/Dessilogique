a6585c01fe53ca6b706f6c2f0b0b85177ce75f23
Méridium <thomas.caillou@laposte.net>
2018-02-23 18:33:32 +0100

    aussi

--- jeu.h
+++ jeu.h
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//renvoie le tableau qui contient les indices des lignes de la grille
 int * IndiceLignes(int n,int m,int ** grille);
 
+//renvoie le tableau qui contient les indices des colonnes de la grille
+int * IndiceColonnes(int n,int m,int ** grille);
