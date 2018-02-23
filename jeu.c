30e4dc09896ea271af775aac6fb20a276cd44e49
Méridium <thomas.caillou@laposte.net>
2018-02-23 19:14:07 +0100

    Changement 2

--- jeu.c
+++ jeu.c
@@ -12,8 +12,8 @@ int** stockage(int** grille, char * nom,int* m,int* n) {
 		fscanf(fichier,&n);
 		fscanf(fichier,&m);
 
-		for (i=0;i<*m;i++) {
-			for (j=0;j<*n;j++) {
+		for (int i=0;i<*n;i++) {
+			for (int j=0;j<*m;j++) {
 				fscanf(fichier, "%s", grille[i][j]);
 			}
 		}
@@ -24,3 +24,50 @@ int** stockage(int** grille, char * nom,int* m,int* n) {
 
 }
 
+int ** IndiceLignes(int n,int m,int ** grille) {
+    int i=0,j=0,indice=0,compteur=0;
+    int indicelign[n][m]=InitialiserTableau(n,m);
+
+    for (i=0;i<n;i++) {
+        j=0;
+        while (j<m) {
+            indice=0;
+
+            while (j<m)&&(0==grille[i][j]) {
+                j++;
+            }
+
+            while (j<m)&&(1==grille[i][j]) {
+                j++;
+                indice++;
+            }
+            indicelign[i][compteur]=indice;
+            compteur++;
+        }
+    }
+    return indicelign;
+}
+
+int ** IndiceColonnes(int n,int m,int ** grille) {
+    int i=0;,j=0,indice=0,compteur=0;
+    int indicecol[m][n]=InitialiserTableau(m,n);
+
+    for (j=0;j<m;j++) {
+        i=0;
+        while (i<n) {
+            indice=0;
+
+            while (i<n)&&(0==grille[i][j]) {
+                i++;
+            }
+
+            while (i<n)&&(1==grille[i][j]) {
+                i++;
+                indice++;
+            }
+            indicecol[j][compteur]=indice;
+            compteur++;
+        }
+    }
+    return indicecol;
+}
